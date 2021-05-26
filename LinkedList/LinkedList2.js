9b29fb8aed41b4158c61a6d95595f250f96026ca
Nur A Alam <nuraalam376@gmail.com>
2021-05-26 13:23:10 +0600

    implemented linked list print list

--- LinkedList/LinkedList2.js
+++ LinkedList/LinkedList2.js
@@ -1,33 +1,49 @@
 // 1 ---> 10 ---> 5 ----> 16
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
+    this.head = newNode;
     this.tail = this.head;
     this.length = 1;
   }
 
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
-
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
+    this.length++;
     return this;
   }
 
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: this.head,
-    };
+    const newNode = new Node(value);
+    newNode.next = this.head;
     this.head = newNode;
+    this.length++;
     return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    const newNode = new Node(value);
   }
 }
 
@@ -35,4 +51,4 @@ const linkedList = new LinkedList(10);
 linkedList.append(5);
 linkedList.append(16);
 linkedList.prepend(1);
-console.log(linkedList);
+console.log(linkedList.printList());
