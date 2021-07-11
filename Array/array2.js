5f42da1070a490a2bf2f305cea6bb2374c5facbb
Nur A Alam <nuraalam376@gmail.com>
2021-07-11 09:34:49 +0600

    implemented linear search

--- Array/array2.js
+++ Array/array2.js
@@ -22,3 +22,22 @@ console.log(strings);
 strings.splice(2, 0, "x"); // O(n)
 
 console.log(strings);
+
+let a = 5;
+let b = 6;
+
+[a, b] = [b, a];
+
+console.log(a, b);
+const array = [1, 2, 3, 4, 5];
+
+const [, , d, e, f] = array;
+console.log(d, e, f);
+
+const arrayObjects = array.map((num) => {
+  return { square: num * num, squrt: Math.sqrt(num) };
+});
+
+console.log(arrayObjects);
+const sum = array.reduce((sum, num) => sum + num, 0);
+console.log(sum);
---
+++ Searching/linearsearch.js
@@ -0,0 +1,9 @@
+const colors = ["Red", "Green", "Blue", "Violet"];
+
+console.log(colors.indexOf("Blue"));
+
+console.log(colors.findIndex((color) => color === "Blue"));
+
+console.log(colors.find((color) => color === "Blue"));
+
+console.log(colors.includes("Blue"));
