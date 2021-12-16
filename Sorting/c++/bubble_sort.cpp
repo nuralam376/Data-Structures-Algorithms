f08c5afa817fddbf7136a0d9d43d693e2e8d7400
Nur A Alam <67529600+nuralam376@users.noreply.github.com>
2021-12-16 11:45:21 +0600

    bubble sort

---
+++ Sorting/c++/bubble_sort.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int *arr, int n) {
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n - i - 1; j++) {
+			if(arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j+1]);
+			}
+		}
+	}
+}
+
+int main() {
+	int arr[] = {-2,1,-12,4,3,2,8};
+	int n = sizeof(arr) / sizeof(int);
+	bubble_sort(arr, n);
+
+	for(int i = 0; i < n; i++) {
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
+
