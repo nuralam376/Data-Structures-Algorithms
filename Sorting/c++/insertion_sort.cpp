5a2cab9edf17e67611b4d3ffee4edf93fc3da792
Nur A Alam <67529600+nuralam376@users.noreply.github.com>
2021-12-16 12:55:03 +0600

    insertion sort

---
+++ Sorting/c++/insertion_sort.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int *arr, int n) {
+	for(int i = 1; i <= n - 1; i++) {
+		int current = arr[i];
+		int prev =  i - 1;
+
+		while(prev >=0 and arr[prev] > current) {
+			arr[prev + 1] = arr[prev];
+			prev = prev - 1;
+		}
+		arr[prev + 1] = current;
+	}
+}
+
+int main() {
+	int arr[] = {-2,1,-12,4,3,2,8};
+	int n = sizeof(arr) / sizeof(int);
+	insertion_sort(arr, n);
+
+	for(int i = 0; i < n; i++) {
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
+
