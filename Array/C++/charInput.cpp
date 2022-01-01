33841c6b9150ecca7402608102f36399268ca743
Nur A Alam <67529600+nuralam376@users.noreply.github.com>
2022-01-01 21:48:52 +0600

    updated char input

--- Array/C++/charInput.cpp
+++ Array/C++/charInput.cpp
@@ -5,11 +5,15 @@ using namespace std;
 int main() {
 	// your code goes here
 	char temp = cin.get();
+	char sentences[100];
+	int len = 0;
 
 	while(temp!='#') {
-		cout<<temp;
+		sentences[len++] = temp;
 		temp = cin.get();
 	}
+	sentences[len] = '\0';
+	cout<<sentences;
 
 	return 0;
 }
