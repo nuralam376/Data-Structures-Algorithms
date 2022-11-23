0d9175de4ff946d60771f149f1fdd79665242d48
Nur A Alam <nuraalam376@gmail.com>
2022-11-23 15:30:18 +0600

    Order agnostic binary search

--- Searching/binarysearch.js
+++ Searching/binarysearch.js
@@ -1,16 +1,21 @@
-const array = [10,20,30,40,50,60,70,80];
-
 function binarySearch(array, target) {
     let start = 0;
     let end = array.length - 1;
     let mid;
+    let order;
+
+    if(array[start] > array[end]) {
+        order = "desc";
+    } else {
+        order = "asc";
+    }
 
     while(start <= end) {
         mid = Math.floor((start + end) / 2);
 
         if(array[mid] === target) {
             return "Data found at position " + mid;
-        } else if(array[mid] < target) {
+        } else if((order === "desc" && array[mid] > target) || (order === "asc" && array[mid] < target)) {
             start = mid + 1;
         } else {
             end = mid - 1;
@@ -19,5 +24,9 @@ function binarySearch(array, target) {
     return "Data not found";
 }
 
+const array = [10,20,30,40,50,60,70,80];
+const array2 = [80, 70, 60, 50, 40, 30, 20, 10];
 const target = 30;
+const target2 = 50;
 console.log(binarySearch(array, target));
+console.log(binarySearch(array2, target2));
