a6d5370531f1de20cf7db404bd5ff3c513b77d88
Nur A Alam <nuraalam376@gmail.com>
2022-11-29 16:41:40 +0600

    Peak Element binary search

---
+++ Searching/PeakElement.js
@@ -0,0 +1,19 @@
+const peakElement = (nums) => {
+    let low = 0;
+    let high = nums.length - 1;
+    let mid;
+
+    while(low < high) {
+        mid = Math.floor((low + high) / 2);
+
+        if(nums[mid] > nums[mid + 1]) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+};
+
+console.log(peakElement([1,2,3,10]));
