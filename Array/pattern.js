7ee49d05aed3f4b36148f8ce023e505ce8ebba9a
Nur A Alam <nuraalam376@gmail.com>
2025-06-09 13:18:54 +0600

    Second largest

--- Array/pattern.js
+++ Array/pattern.js
@@ -85,6 +85,25 @@ function fifthNumberPattern(n) {
         console.log(row);
     }
 }
+
+function secondLargest(arr) {
+    if(arr.length < 2) {
+        return "Arrays should have at least two elements";
+    }
+    let firstLargest = -Infinity;
+    let secondLargest = Infinity;
+
+    for(let i = 0 ; i < arr.length; i++) {
+        if(arr[i] > firstLargest) {
+            secondLargest = firstLargest;
+            firstLargest = arr[i];
+        } else if(arr[i] > secondLargest && arr[i] !== firstLargest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    return secondLargest === -Infinity ?  "No second largets found" : secondLargest;
+}
 // firstStarPattern(4);
 // secondStarPattern(4);
 // thridStarPattern(4);
@@ -92,4 +111,5 @@ function fifthNumberPattern(n) {
 // secondNumberPattern(4);
 // thridNumberPattern(4);
 // fourthNumberPattern(4);
-fifthNumberPattern(4);
+// fifthNumberPattern(4);
+console.log(secondLargest([1,2,4,3,9,7,13,8]));
