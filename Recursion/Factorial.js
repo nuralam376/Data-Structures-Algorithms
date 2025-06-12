d48ce6e6591ffbc87fcd3fc18e1cb3c498271f19
Nur A Alam <nuraalam376@gmail.com>
2025-06-12 18:48:15 +0600

    Factorial

--- Recursion/Factorial.js
+++ Recursion/Factorial.js
@@ -17,5 +17,11 @@ function findFactorialIterative(number) {
   return answer;
 }
 
-console.log(findFactorialRecursive(5));
-console.log(findFactorialIterative(-4));
+function factorial(n) {
+  if(n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+// console.log(findFactorialRecursive(5));
+// console.log(findFactorialIterative(-4));
+console.log(factorial(5));
