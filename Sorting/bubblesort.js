199c0268e6f7299486d62585c9ab02d8a0b13ee8
Nur A Alam <nuraalam376@gmail.com>
2025-06-21 21:39:20 +0600

    Bubble sort modifications

--- Sorting/bubblesort.js
+++ Sorting/bubblesort.js
@@ -1,15 +1,22 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
-  const arrayLength = array.length;
-  for (let index = 0; index < arrayLength; index++) {
-    for (index2 = 0; index2 < arrayLength; index2++) {
-      if (array[index2] > array[index2 + 1]) {
-        let temp = array[index2]; // 99
-        array[index2] = array[index2 + 1]; // 99
-        array[index2 + 1] = temp; // 44
+  const n = array.length;
+
+  for(let i = 0; i < n - 1; i++) {
+    let isSwapped = false;
+
+    for(let j = 0; j < n - 1 - i; j++) {
+      if(array[j] > array[j + 1]) {
+        let temp = array[j + 1]; // 99
+        array[j + 1] = array[j]; // 99
+        array[j] = temp; 
+
+        isSwapped = true;
       }
     }
+
+    if(!isSwapped) return;
   }
   return array;
 }
