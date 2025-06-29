bbb73f5ba888f1d8fb813b9cf8e9434f25147460
Nur A Alam <nuraalam376@gmail.com>
2025-06-29 12:13:29 +0600

    Insertion sort implementation

--- Sorting/insertionsort.js
+++ Sorting/insertionsort.js
@@ -2,15 +2,18 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function insertionSort(array) {
   const arrayLength = array.length;
-  for (let index = 0; index < arrayLength; index++) {
-    if (array[index] < array[0]) {
-      array.unshift(array.splice(index, 1)[0]);
-    } else {
-      for (let index2 = 1; index2 < index; index2++) {
-        if (array[index] > array[index2 - 1] && array[index] < array[index2]) {
-          array.splice(index2, 0, array.splice(index, 1)[0]);
-        }
+  for(let i = 0; i < arrayLength - 1; i++) {
+    let min = i;
+    for(let j = i + 1; j < arrayLength; j++) {
+      if(array[j] < array[min]) {
+        min = j;
       }
+    }
+
+    if(min !== i) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
     }
   }
 }
